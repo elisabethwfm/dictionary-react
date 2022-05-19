a49c808005d566d994194358ec2a9aff0fdaccfb
elisabethwfm <elisabeth.wolfram@gmail.com>
2022-05-19 14:16:00 +0200

    making preparations for the meanings component

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 export default function Results(props) {
+  console.log(props.results);
   if (props.results) {
     return (
       <div className="results">
         <h2>
           Word - <span className="word">{props.results.word}</span>
         </h2>
+        {props.results.meanings.map(function (meanings, index) {
+          return;
+          <div key={index} className="meanings">
+            return meanings.definitions[0].definition;
+          </div>;
+        })}
       </div>
     );
   } else {
