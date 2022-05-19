46591c1ae669abc031f1fcab02f81aa4ba01a6f5
elisabethwfm <elisabeth.wolfram@gmail.com>
2022-05-19 13:32:21 +0200

    created a results component and included a test run

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import DictionarySearch from "./DictionarySearch";
+import Results from "./Results";
 
 function App() {
   return (
     <div className="App">
       <DictionarySearch />
+      <Results />
     </div>
   );
 }
